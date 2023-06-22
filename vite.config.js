19ce8beb969e669a2fe8fca99eb86ec5e94e75d0
Karam <karamjoudah@hotmail.com>
2023-06-22 00:56:49 -0400

    deploying to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/newsletter-sign-up-with-sucess-message-main/'
+      : '/',
+});
